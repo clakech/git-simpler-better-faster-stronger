831f929b01d53266b884a9641eec59cf49061bc2
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2014-04-15 01:33:44 +0200

    update scripts to latest demo

--- git-scripts/01-02-commit.js
+++ git-scripts/01-02-commit.js
@@ -4,14 +4,13 @@ var git = require('./git.js');
 
 var badToGoodCommits = [
 	{ bad: 'design', good: 'feat: apply custom design\n\ncustom design is light for clarity purposes, colors are chosen from the official Devoxx 2014 templates : http://www.devoxx.fr/2014/03/modeles-de-presentation-pour-devoxx-france-2014/ ' },
-	{ bad: 'add good history contents', good: 'feat(history): list reasons why a clean history matters' },
-	{ bad: 'commit message example', good: 'feat(conventions): show full commit message example following conventions' },
-	{ bad: 'questions for conventions', good: 'feat(conventions): list questions that should be answered by a good commit message' },
-	{ bad: 'rebase -i', good: 'feat(rebaseInteractive): recap commands to alter commits' },
-	{ bad: 'changelog', good: 'feat(changelog): show a generated changelog example' },
-	{ bad: 'merge vs rebase', good: 'feat(mergeVsRebase): recap pros and cons' },
-	{ bad: 'THX and Q&A', good: 'feat(ending): thank audience and invite to Q&A' },
-	{ bad: 'changelog image', good: 'refactor(changelog): replace text with an image\n\nthe generated HTML example as an image is prettier to display than the markdown version' }
+	{ bad: 'belief', good: 'feat(belief): show a clean history graph' },
+	{ bad: 'conventions', good: 'feat(method): show full commit message example following conventions' },
+	{ bad: 'rebase vs merge', good: 'feat(method): show merge vs rebase clean/dirty image' },
+	{ bad: 'manipulate history', good: 'feat(tools): recap actions to manipulate history' },
+	{ bad: 'transitions', good: 'refactor: use classic and fast transitions' },
+	{ bad: 'changelog', good: 'feat(tools): show a generated changelog example' },
+	{ bad: 'THX and Q&A', good: 'feat: thank audience and invite to Q&A' }
 ];
 
 git.replace(function (data) {
--- git-scripts/goto.sh
+++ git-scripts/goto.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-GIT_SEQUENCE_EDITOR=~/dev/git-scripts/$1-alter-todo.js GIT_EDITOR=~/dev/git-scripts/$1-commit.js git rebase -i master
+GIT_SEQUENCE_EDITOR=~/dev/git-simpler-better-faster-stronger/git-scripts/$1-alter-todo.js GIT_EDITOR=~/dev/git-simpler-better-faster-stronger/git-scripts/$1-commit.js git rebase -i master
